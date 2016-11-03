5e779153736138c5a1ce0b47939fec5795c0c414
Mihir Tiwari <mtiw999@gmail.com>
2016-11-02 22:45:51 -0400

    managed to separate file names from extensions. now need to install libraries for scraping and stuff

--- moviedata/MovieCrawler.py
+++ moviedata/MovieCrawler.py
@@ -5,18 +5,8 @@ path = raw_input("Please put full path to directory with movie files\n")
 
 os.chdir(path)
 
-num_mp4 = 0
-num_mkv = 0
-num_avi = 0
-
 for file in os.listdir(path):
-    if file.endswith(".mp4"):
-        num_mp4 += 1
-    elif file.endswith(".mkv"):
-        num_mkv += 1
-    elif file.endswith(".avi"):
-        num_avi += 1
+    name = file.title()
+    name = name[0:name.index(".")]
 
-print "Number of .mp4: " + str(num_mp4)
-print "Number of .mkv: " + str(num_mkv)
-print "Number of .avi: " + str(num_avi)
+    print name
